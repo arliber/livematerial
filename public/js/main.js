e3c907fba76fc501d89684fc6fa164510cf49c4c
Arik Liber <leonliber@gmail.com>
2016-10-29 22:53:21 +0300

    Text updates Added campaign overlay message

--- public/js/main.js
+++ public/js/main.js
@@ -11,39 +11,49 @@ $(function () {
           allowClear: true
     });
 
-    Dropzone.options.campaignWizard = {
-      maxFiles: 8,
-      acceptedFiles: 'image/*',
-      autoProcessQueue: false,
-      uploadMultiple: true,
-      clickable: false,
-      parallelUploads: 100,
-      previewTemplate: '<div class="file-attachment"><button data-dz-remove><span data-dz-name></span></button></div>',
-      previewsContainer: '#files-container',
-      createImageThumbnails: false,
+    $("#campaign-wizard-overlay button").click(function(){
+        var $elm = $("#campaign-wizard-overlay");
+        $elm.fadeTo('slow', 0, function() {
+            $elm.css('display', 'none');
+        });
+    });
 
-      init: function() {
-        var myDropzone = this;
+    Dropzone.options.campaignWizardForm = {
+        maxFiles: 8,
+        acceptedFiles: 'image/*',
+        autoProcessQueue: false,
+        uploadMultiple: true,
+        clickable: false,
+        parallelUploads: 100,
+        previewTemplate: '<div class="file-attachment"><button data-dz-remove><span data-dz-name></span></button></div>',
+        previewsContainer: '#files-container',
+        createImageThumbnails: false,
 
-        this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
-            console.log('Submitting form..');
+        init: function() {
+            var myDropzone = this;
 
-            e.preventDefault();
-            e.stopPropagation();
+            this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+                console.log('Submitting form..');
 
-            if (myDropzone.getQueuedFiles().length > 0) {
-                console.log('Sending with files..');
-                myDropzone.processQueue();
-            } else {
-                console.log('Sending with NO files..');
-                var mockFile =   {
-                    name: "mock",
-                    size: 1,
-                    type: 'image/jpeg',
-                    status: Dropzone.ADDED
-                };
-                myDropzone.uploadFiles([mockFile]); //send empty
-            }
+                e.preventDefault();
+                e.stopPropagation();
+
+                if (myDropzone.getQueuedFiles().length > 0) {
+                    console.log('Sending with files..');
+                    myDropzone.processQueue();
+                } else {
+                    console.log('Sending with NO files..');
+                    var mockFile =   {
+                        name: "mock",
+                        size: 1,
+                        type: 'image/jpeg',
+                        status: Dropzone.ADDED
+                    };
+                    myDropzone.uploadFiles([mockFile]); //send empty
+                }
+
+                $("#campaign-wizard-overlay").css('display', 'flex').fadeTo('slow', 1);ss('opacity', 0);
+
         });
       },
         drop: function() {
