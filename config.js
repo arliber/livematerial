9a7ed7b271c4f0b03e103be6cece11e01c248eed
Arik Liber <leonliber@gmail.com>
2017-11-18 23:35:00 +0200

    add screenshot, restore config with no passwords

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  port: 3000,
+  baseUrl: 'http://localhost:3000/',
+  connectionString: 'mongodb://localhost:27017/livermaterial',
+  mailgun: {
+    apiKey: '<your-key>', 
+    domain: 'livematerial.io'
+  }
+};
